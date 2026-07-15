5d8103090c2ea2939136020b1b996519d92a3cc3
Spiritus93 <88580641+Spiritus93@users.noreply.github.com>
2026-07-15 10:20:03 +0200

    Add primary button

---
+++ src/Components/Button/Button.jsx
@@ -0,0 +1,8 @@
+import "./Button.scss";
+
+const Button = (props) => {
+    return (
+        <button className={props.classList} onClick={props.onClick}>{props.title}</button>
+    )
+}
+export default Button;
